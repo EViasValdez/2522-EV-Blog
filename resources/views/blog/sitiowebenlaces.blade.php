75427e344432abaa0d77233f48c0da6ed8e569dd
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-04-10 10:51:02 -0600

    Add files via upload

--- resources/views/blog/sitiowebenlaces.blade.php
+++ resources/views/blog/sitiowebenlaces.blade.php
@@ -21,13 +21,13 @@
             padding-top: 10px;
             background-color: rgb(28, 0, 134);
         }
-        #navegador {
+        #Navegador {
             float: top;
             font-family: "Permanent Marker";
             text-align: center;
             background-color: rgb(217, 0, 0);
         }
-        #opcinavg {
+        #OpciNavg {
             background-color: rgb(217, 0, 0);
             color: white;
             text-decoration: none;
@@ -38,7 +38,7 @@
             transition-timing-function: ease-in-out;
             transition: 0.8s;
         }
-        #opcinavg:hover {
+        #OpciNavg:hover {
             background-color: rgb(255, 133, 133);
             color: black;
             text-decoration: none;
@@ -49,41 +49,41 @@
             transition-timing-function: ease-in-out;
             transition: 0.4s;
         }
-        #titulo {
+        #Titulo {
             text-align: center;
             font-family: "Allerta Stencil";
             font-size: 48px;
             font-weight: bolder;
             color: white;
         }
-        #seccion {
+        #Seccion {
             text-align: center;
             font-size: 40px;
             font-family: "Ramabhadra";
             color: white;
         }
-        #titulo {
+        #Titulo {
             text-align: center;
             font-weight: bold;
             font-family: "Saira";
             font-size: 38px;
             color: white;
         }
-        #txt {
+        #Txt {
             text-align: center;
             font-weight: bold;
             font-family: "Saira";
             font-size: 24px;
             color: white;
         }
-        #txts {
+        #Txts {
             text-align: center;
             font-weight: bold;
             font-family: "Saira";
             font-size: 20px;
             color: white;
         }
-        #enlace {
+        #Enlace {
             display: block;
             margin-left: auto;
             margin-right: auto;
@@ -93,55 +93,55 @@
             font-size: 20px;
             color: white;
         }
-        #vs22logo {
+        #Vs22Logo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #vsclogo {
+        #VscLogo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #vscodiumlogo {
+        #VsCodiumLogo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #nblogo {
+        #NBlogo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #xplogo {
+        #XPLogo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #laravellogo {
+        #LaravelLogo {
             margin: 0px auto;
             display: block;
             height: 200px;
             width: 200px;
         }
-        #complogo {
+        #CompLogo {
             margin: 0px auto;
             display: block;
             height: 120px;
             width: 100px;
         }
-        #jquelogo {
+        #JQueLogo {
             margin: 0px auto;
             display: block;
             height: 100px;
             width: 100px;
         }
-        #bootstrlogo {
+        #BootStrLogo {
             margin: 0px auto;
             display: block;
             height: 100px;
@@ -149,57 +149,57 @@
         }
     </style>
         <header>
-            <p id="titulo">Salon de la programación</p>
-            <div id="navegador">
-                <a id="opcinavg" href="sitiowebinicio">Inicio</a>
-                <a id="opcinavg" href="sitiowebact">Actualizaciones</a>
-                <a id="opcinavg" href="">Experimentación</a>
-                <a id="opcinavg" href="sitiowebenlaces">Enlaces</a>
-                <a id="opcinavg" href="">Otros</a>
+            <p id="Titulo">Salon de la programación</p>
+            <div id="Navegador">
+                <a id="OpciNavg" href="sitiowebinicio">Inicio</a>
+                <a id="OpciNavg" href="sitiowebact">Actualizaciones</a>
+                <a id="OpciNavg" href="">Experimentación</a>
+                <a id="OpciNavg" href="sitiowebenlaces">Enlaces</a>
+                <a id="OpciNavg" href="">Otros</a>
             </div>
         </header>
-    <p id="seccion">Enlaces</p>
-    <p id="txts">Aquí pongo algunos materiales que yo uso para la materia y que quizás les puedan ser utiles</p>
-    <p id="titulo">Entornos de programación usados</p>
-    <p id="txt">Visual Studio Code</p>
-    <img id="vsclogo" src="https://www.returngis.net/wp-content/uploads/2015/11/VS-Code.png">
-    <p id="txts">Principal editor que utilizo para casi todos los lenguajes</p>
-    <a id="enlace" href="https://code.visualstudio.com/">https://code.visualstudio.com/</a>
-    <p id="txt">VSCodium</p>
-    <img id="vscodiumlogo" src="https://user-images.githubusercontent.com/1499476/90325153-1cac9a80-df46-11ea-967a-486d523f551b.png">
-    <p id="txts">Por agregar.</p>
-    <a id="enlace" href="https://vscodium.com/">https://vscodium.com/</a>
-    <p id="txt">Visual Studio 2022</p>
-    <img id="vs22logo" src="https://www.megaleechers.com/storage/Visual-Studio-2022-Icon.png">
-    <p id="txts">Editor secundario que uso para ciertos lenguajes o modalidades como los forms</p>
-    <a id="enlace" href="https://visualstudio.microsoft.com/es/vs/community/">https://visualstudio.microsoft.com/es/vs/community/</a>
-    <p id="txts">NetBeans 8.2</p>
-    <img id="nblogo" src="https://1.bp.blogspot.com/_BQk94ojgkDY/TK4VWFA_WVI/AAAAAAAADHI/2ZMfQgswmjw/s1600/netbeans-logo.png">
-    <p id="txts">Programa para realizar una que otra cosa de Java, rara vez lo uso actualmente</p>
-    <a id="enlace" href="https://netbeans.apache.org/download/archive/index.html">https://netbeans.apache.org/download/archive/index.html</a>
-    <a id="enlace" href="https://archive.org/details/jdk-8u111-nb-8_2">https://archive.org/details/jdk-8u111-nb-8_2</a>
+    <p id="Seccion">Enlaces</p>
+    <p id="Txts">Aquí pongo algunos materiales que yo uso para la materia y que quizás les puedan ser utiles</p>
+    <p id="Titulo">Entornos de programación usados</p>
+    <p id="Txt">Visual Studio Code</p>
+    <img id="VscLogo" src="https://www.returngis.net/wp-content/uploads/2015/11/VS-Code.png">
+    <p id="Txts">Principal editor que utilizo para casi todos los lenguajes</p>
+    <a id="Enlace" href="https://code.visualstudio.com/">https://code.visualstudio.com/</a>
+    <p id="Txt">VSCodium</p>
+    <img id="VsCodiumlogo" src="https://user-images.githubusercontent.com/1499476/90325153-1cac9a80-df46-11ea-967a-486d523f551b.png">
+    <p id="Txts">Por agregar.</p>
+    <a id="Enlace" href="https://vscodium.com/">https://vscodium.com/</a>
+    <p id="Txt">Visual Studio 2022</p>
+    <img id="Vs22Logo" src="https://www.megaleechers.com/storage/Visual-Studio-2022-Icon.png">
+    <p id="Txts">Editor secundario que uso para ciertos lenguajes o modalidades como los forms</p>
+    <a id="Enlace" href="https://visualstudio.microsoft.com/es/vs/community/">https://visualstudio.microsoft.com/es/vs/community/</a>
+    <p id="Txts">NetBeans 8.2</p>
+    <img id="NBlogo" src="https://1.bp.blogspot.com/_BQk94ojgkDY/TK4VWFA_WVI/AAAAAAAADHI/2ZMfQgswmjw/s1600/netbeans-logo.png">
+    <p id="Txts">Programa para realizar una que otra cosa de Java, rara vez lo uso actualmente</p>
+    <a id="Enlace" href="https://netbeans.apache.org/download/archive/index.html">https://netbeans.apache.org/download/archive/index.html</a>
+    <a id="Enlace" href="https://archive.org/details/jdk-8u111-nb-8_2">https://archive.org/details/jdk-8u111-nb-8_2</a>
     <br>
     <br>
-    <p id="titulo">Programas / herramientas</p>
-    <p id="txt">XAMPP</p>
-    <img id="xplogo" src="https://luiscobian.files.wordpress.com/2013/05/xampp.jpg">
-    <p id="txts">Creo que uso mucho este gestor</p>
-    <a id="enlace" href="https://www.apachefriends.org/download.html">https://www.apachefriends.org/download.html</a>
-    <p id="txt">Laravel</p>
-    <img id="laravellogo" src="https://logospng.org/download/laravel/logo-laravel-1024.png">
-    <p id="txts">Este también lo uso mas en estos dias</p>
-    <a id="enlace" href="https://laravel.com/docs/4.2">https://laravel.com/docs/4.2</a>
-    <p id="txt">Composer</p>
-    <img id="complogo" src="https://cdn.freebiesupply.com/logos/large/2x/composer-logo-png-transparent.png">
-    <p id="txts">Lo necesitaran para instalar laravel</p>
-    <a id="enlace" href="https://getcomposer.org/download/">https://getcomposer.org/download/</a>
-    <p id="txt">qQuery</p>
-    <img id="jquelogo" src="https://icon-library.com/images/jquery-icon-png/jquery-icon-png-7.jpg">
-    <p id="txts">Usado para ciertos proyectos que hice en programación web</p>
-    <a id="enlace" href="https://jquery.com/download/">https://jquery.com/download/</a>
-    <p id="txt">Bootstrap 5</p>
-    <img id="bootstrlogo" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg">
-    <p id="txts">Creo que lo use en algunos trabajos, eso si es complicado de manejar</p>
-    <a id="enlace" href="https://getbootstrap.com/docs/5.1/getting-started/download/">https://getbootstrap.com/docs/5.1/getting-started/download/</a>
+    <p id="Titulo">Programas / herramientas</p>
+    <p id="Txt">XAMPP</p>
+    <img id="XPLogo" src="https://luiscobian.files.wordpress.com/2013/05/xampp.jpg">
+    <p id="Txts">Creo que uso mucho este gestor</p>
+    <a id="Enlace" href="https://www.apachefriends.org/download.html">https://www.apachefriends.org/download.html</a>
+    <p id="Txt">Laravel</p>
+    <img id="LaravelLogo" src="https://logospng.org/download/laravel/logo-laravel-1024.png">
+    <p id="Txts">Este también lo uso mas en estos dias</p>
+    <a id="Enlace" href="https://laravel.com/docs/4.2">https://laravel.com/docs/4.2</a>
+    <p id="Txt">Composer</p>
+    <img id="CompLogo" src="https://cdn.freebiesupply.com/logos/large/2x/composer-logo-png-transparent.png">
+    <p id="Txts">Lo necesitaran para instalar laravel</p>
+    <a id="Enlace" href="https://getcomposer.org/download/">https://getcomposer.org/download/</a>
+    <p id="Txt">qQuery</p>
+    <img id="JQueLogo" src="https://icon-library.com/images/jquery-icon-png/jquery-icon-png-7.jpg">
+    <p id="Txts">Usado para ciertos proyectos que hice en programación web</p>
+    <a id="Enlace" href="https://jquery.com/download/">https://jquery.com/download/</a>
+    <p id="Txt">Bootstrap 5</p>
+    <img id="BootStrLogo" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg">
+    <p id="Txts">Creo que lo use en algunos trabajos, eso si es complicado de manejar</p>
+    <a id="Enlace" href="https://getbootstrap.com/docs/5.1/getting-started/download/">https://getbootstrap.com/docs/5.1/getting-started/download/</a>
 </body>
-</html> 
+</html>
