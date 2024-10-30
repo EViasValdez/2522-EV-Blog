9f77aaec53e3bb12181afd8dbd6788431427734e
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-10-29 21:52:22 -0600

    Add files via upload

---
+++ resources/views/blog/SitioWebEnlaces.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Saira&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Allerta+Stencil&family=Saira&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ramabhadra&display=swap" rel="stylesheet">
+    <title>R.</title>
+</head>
+<body>
+    <style type="text/css">
+        body {
+            background-color: #000090;
+            background-image: linear-gradient(to right, #000000, #000000 20px, #000090 20px, #000090 );
+            background-size: 40px 100%;
+        }
+        header {
+            padding-top: 10px;
+            background-color: rgba(28, 0, 134);
+        }
+        #Navegador {
+            float: top;
+            font-family: "Permanent Marker";
+            text-align: center;
+            background-color: rgba(217, 0, 0);
+        }
+        #OpciNavg {
+            background-color: rgba(217, 0, 0);
+            color: rgba(255, 255, 255);
+            text-decoration: none;
+            text-align: center;
+            font-size: 28px;
+            padding-left: 48px;
+            padding-right: 48px;
+            transition-timing-function: ease-in-out;
+            transition: 0.8s;
+        }
+        #OpciNavg:hover {
+            background-color: rgba(255, 133, 133);
+            color: rgba(255, 255, 255);
+            text-decoration: none;
+            text-align: center;
+            font-size: 28px;
+            padding-left: 148px;
+            padding-right: 148px;
+            transition-timing-function: ease-in-out;
+            transition: 0.4s;
+        }
+        #Titulo {
+            text-align: center;
+            font-family: "Allerta Stencil";
+            font-size: 48px;
+            font-weight: bolder;
+            color: rgba(255, 255, 255);
+        }
+        #Seccion {
+            text-align: center;
+            font-size: 40px;
+            font-family: "Ramabhadra";
+            color: rgba(255, 255, 255);
+        }
+        #Titulo {
+            text-align: center;
+            font-weight: bold;
+            font-family: "Saira";
+            font-size: 38px;
+            color: rgba(255, 255, 255);
+        }
+        #Txt {
+            text-align: center;
+            font-weight: bold;
+            font-family: "Saira";
+            font-size: 24px;
+            color: rgba(255, 255, 255);
+        }
+        #Txts {
+            text-align: center;
+            font-weight: bold;
+            font-family: "Saira";
+            font-size: 20px;
+            color: rgba(255, 255, 255);
+        }
+        #Enlace {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+            font-weight: bold;
+            font-family: "Saira";
+            font-size: 20px;
+            color: rgba(255, 255, 255);
+        }
+        #Vs22Logo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #VscLogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #VsCodiumLogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #NBlogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #XPLogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #LaravelLogo {
+            margin: 0px auto;
+            display: block;
+            height: 200px;
+            width: 200px;
+        }
+        #CompLogo {
+            margin: 0px auto;
+            display: block;
+            height: 120px;
+            width: 100px;
+        }
+        #JQueLogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 100px;
+        }
+        #BootStrLogo {
+            margin: 0px auto;
+            display: block;
+            height: 100px;
+            width: 128px;
+        }
+    </style>
+        <header>
+            <p id="Titulo">Salon de la programación</p>
+            <div id="Navegador">
+                <a id="OpciNavg" href="Index.html">Inicio</a>
+                <a id="OpciNavg" href="SitioWebAct.html">Actualizaciones</a>
+                <a id="OpciNavg" href="">Experimentación</a>
+                <a id="OpciNavg" href="SitioWebEnlaces.html">Enlaces</a>
+                <a id="OpciNavg" href="">Otros</a>
+            </div>
+        </header>
+    <p id="Seccion">Enlaces</p>
+    <p id="Txts">Aquí pongo algunos materiales que yo uso para la materia y que quizás les puedan ser utiles</p>
+    <p id="Titulo">Entornos de programación usados</p>
+    <p id="Txt">Visual Studio Code</p>
+    <img id="VscLogo" src="https://www.returngis.net/wp-content/uploads/2015/11/VS-Code.png">
+    <p id="Txts">Principal editor que utilizo para casi todos los lenguajes</p>
+    <a id="Enlace" href="https://code.visualstudio.com/">https://code.visualstudio.com/</a>
+    <p id="Txt">VSCodium</p>
+    <img id="VsCodiumlogo" src="https://user-images.githubusercontent.com/1499476/90325153-1cac9a80-df46-11ea-967a-486d523f551b.png">
+    <p id="Txts">Por agregar.</p>
+    <a id="Enlace" href="https://vscodium.com/">https://vscodium.com/</a>
+    <p id="Txt">Visual Studio 2022</p>
+    <img id="Vs22Logo" src="https://www.megaleechers.com/storage/Visual-Studio-2022-Icon.png">
+    <p id="Txts">Editor secundario que uso para ciertos lenguajes o modalidades como los forms</p>
+    <a id="Enlace" href="https://visualstudio.microsoft.com/es/vs/community/">https://visualstudio.microsoft.com/es/vs/community/</a>
+    <p id="Txts">NetBeans 8.2</p>
+    <img id="NBlogo" src="https://1.bp.blogspot.com/_BQk94ojgkDY/TK4VWFA_WVI/AAAAAAAADHI/2ZMfQgswmjw/s1600/netbeans-logo.png">
+    <p id="Txts">Programa para realizar una que otra cosa de Java, rara vez lo uso actualmente</p>
+    <a id="Enlace" href="https://netbeans.apache.org/download/archive/index.html">https://netbeans.apache.org/download/archive/index.html</a>
+    <a id="Enlace" href="https://archive.org/details/jdk-8u111-nb-8_2">https://archive.org/details/jdk-8u111-nb-8_2</a>
+    <br>
+    <br>
+    <p id="Titulo">Programas / herramientas</p>
+    <p id="Txt">XAMPP</p>
+    <img id="XPLogo" src="https://luiscobian.files.wordpress.com/2013/05/xampp.jpg">
+    <p id="Txts">Creo que uso mucho este gestor</p>
+    <a id="Enlace" href="https://www.apachefriends.org/download.html">https://www.apachefriends.org/download.html</a>
+    <p id="Txt">Laravel</p>
+    <img id="LaravelLogo" src="https://logospng.org/download/laravel/logo-laravel-1024.png">
+    <p id="Txts">Este también lo uso mas en estos dias</p>
+    <a id="Enlace" href="https://laravel.com/docs/4.2">https://laravel.com/docs/4.2</a>
+    <p id="Txt">Composer</p>
+    <img id="CompLogo" src="https://cdn.freebiesupply.com/logos/large/2x/composer-logo-png-transparent.png">
+    <p id="Txts">Lo necesitaran para instalar laravel</p>
+    <a id="Enlace" href="https://getcomposer.org/download/">https://getcomposer.org/download/</a>
+    <p id="Txt">JQuery</p>
+    <img id="JQueLogo" src="https://icon-library.com/images/jquery-icon-png/jquery-icon-png-7.jpg">
+    <p id="Txts">Usado para ciertos proyectos que hice en programación web</p>
+    <a id="Enlace" href="https://jquery.com/download/">https://jquery.com/download/</a>
+    <p id="Txt">Bootstrap 5</p>
+    <img id="BootStrLogo" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg">
+    <p id="Txts">Creo que lo use en algunos trabajos, eso si es complicado de manejar</p>
+    <a id="Enlace" href="https://getbootstrap.com/docs/5.1/getting-started/download/">https://getbootstrap.com/docs/5.1/getting-started/download/</a>
+</body>
+</html>
